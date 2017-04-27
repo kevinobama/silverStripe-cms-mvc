e6941e2ff618eac9260d5ab6f9550dcfc453c9f3
Kevin Gates <picture654202@yahoo.com>
2017-04-27 10:49:47 +0800

    update

--- mysite/code/model/Walkthrough.php
+++ mysite/code/model/Walkthrough.php
@@ -3,11 +3,13 @@ class Walkthrough extends DataObject  {
 
 		private static $db = array(
 	        'Text' => 'Text',
-            'Sort' => 'Int'
+            'Sort' => 'Int',
+            'Page' => 'Varchar(255)'
 	    );
 
         private static $defaults = array(
             'Sort' => 0,
+            'Page' => 'all',
         );
 
 	    private static $summary_fields = array(
