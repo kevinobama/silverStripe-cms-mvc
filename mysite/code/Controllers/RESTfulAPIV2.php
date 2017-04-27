b69adfa899510d7adce2f7795c6f739d9bc0ceac
Kevin Gates <picture654202@yahoo.com>
2017-04-27 11:16:25 +0800

    update

--- mysite/code/Controllers/RESTfulAPIV2.php
+++ mysite/code/Controllers/RESTfulAPIV2.php
@@ -4,16 +4,24 @@ use Lame\Settings;
 
 class RESTfulAPIV2 extends Controller
 {
-    private static $allowed_actions = array('faskcreate','walkthrough');
+    private static $allowed_actions = array('walkthrough');
 
     public function walkthrough(SS_HTTPRequest $r)
     {
-        $json['Text'] = null;
-        $walkthrough = Walkthrough::get()->byID(1);
-        if($walkthrough && $walkthrough->Text) {
-            $json['Text'] = $walkthrough->Text; 
+        $limit = 100;
+        $json['walkthroughs'] = array();
+        $walkthroughs = Walkthrough::get()
+            ->filter(array('Page' => 'all'))
+            ->sort('Sort', 'ASC')
+            ->limit($limit);
+
+        foreach($walkthroughs as $walkthrough) {
+            $json['walkthroughs'][] = array(
+                'ID'           => $walkthrough->ID,
+                'Text'         => $walkthrough->Text
+            );
         }
-        
-        return new SS_HTTPResponse(Convert::array2json($json), 200);        
+
+        return new SS_HTTPResponse(Convert::array2json($json), 200);
     }
 }
